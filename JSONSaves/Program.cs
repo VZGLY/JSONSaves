317bb8d4c2edfd5b6b6b6025ac8fc465b76a5790
BS-GavinC <98208007+VZGLY@users.noreply.github.com>
2023-06-27 11:57:45 +0200

    init

---
+++ JSONSaves/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+using System.Text.Json;
+using JSONSaves;
+
+
+User? user = null;
+try
+{
+    string save = File.ReadAllText("./save.json");
+    Console.WriteLine($"Fichier : {save}");
+    user = JsonSerializer.Deserialize<User>(save);
+}
+catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+if (user is null)
+{
+    Console.WriteLine("Entrez votre nom");
+    
+    string name = Console.ReadLine();
+    
+    Console.WriteLine("Entrez votre Prenom");
+    
+    string firstname = Console.ReadLine();
+    
+    Console.WriteLine("Entrez votre id");
+    
+    int id = Int32.Parse(Console.ReadLine());
+
+    user = new User(id, name, firstname);
+
+    string jsonUser = JsonSerializer.Serialize(user);
+    
+    
+    
+    File.WriteAllText("./save.json", jsonUser );
+}
+
+Console.WriteLine($"Bonjour {user.Prenom} {user.Nom}, votre id est le {user.id}");
---
+++ JSONSaves/User.cs
@@ -0,0 +1,24 @@
+﻿namespace JSONSaves;
+
+public class User
+{
+
+    public int id;
+
+    public User()
+    {
+        
+    }
+    public User(int id, string nom, string prenom)
+    {
+        this.id = id;
+        Nom = nom;
+        Prenom = prenom;
+    }
+
+    public string Nom { get; set; }
+
+    public string Prenom { get; set; }
+    
+    
+}
